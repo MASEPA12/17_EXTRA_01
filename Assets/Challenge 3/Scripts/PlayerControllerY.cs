44c7945839ccc26372b7ac052e55f5d3771d6722
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-02-02 14:52:00 +0100

    2

---
+++ Assets/Challenge 3/Scripts/PlayerControllerY.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerY : MonoBehaviour
+{
+    public bool gameOver;
+
+    [SerializeField] private int points;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        // Apply a small upward force at the start of the game
+        //playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+        //if the y pos is lower than 0 or greater than 16, rigidbody velocity is 0
+        if (transform.position.y > 16)
+        {
+            playerRb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("ground"))
+        {
+            Debug.Log("Game Over!");
+            Time.timeScale = 0;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // if player collides with money, fireworks
+        if (other.gameObject.CompareTag("Money"))
+        {
+            points++;
+            Debug.Log($"TOTAL SCORE: {points}");
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Challenge 3/Scripts/SpawnManagerY.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerY : MonoBehaviour
+{
+    public GameObject[] objectPrefabs;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
+    private PlayerControllerY playerControllerScript;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
+        playerControllerScript = FindObjectOfType<PlayerControllerY>();
+    }
+
+    // Spawn obstacles
+    void SpawnObjects()
+    {
+        // Set random spawn location and random object index
+        // x= random number between A, B --> A=random number between -14,-12 / B=random btween 12,14
+
+        int L = Random.Range(-14, -12);
+        int R = Random.Range(12, 14);
+
+        Vector3 spawnLocation = new Vector3(Random.Range(Random.Range(-14,-12),Random.Range(12,14)), Random.Range(-7, 7), 0);
+
+        int index = Random.Range(0, objectPrefabs.Length);
+        // If game is still active, spawn new object
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
+    }
+}
